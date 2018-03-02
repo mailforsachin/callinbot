7b0bf8ed19bbaa88ae777db049f1954a8f5f2eab
mailforsachin <mailforsachin@gmail.com>
2018-03-02 11:42:07 +0530

    new filesfa
    test

---
+++ index.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var calling = require('botbuilder-calling');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log(`${server.name} listening to ${server.url}`); 
+});
+
+// Create calling bot
+var connector = new calling.CallConnector({
+    callbackUrl: 'https://<your host>/api/calls',
+    appId: '73762026-a2b2-4470-93c4-f8827610a931',
+    appPassword: 'yhevvIKV11(*dlBAZT566|^'
+});
+var bot = new calling.UniversalCallBot(connector);
+server.post('/api/calls', connector.listen());
+
+// Add root dialog
+bot.dialog('/', function (session) {
+    session.send('Watson... come here!');
+});
